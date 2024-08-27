64a5988e6d55e2a2b7ab5617c4cbcd730e63b620
Alsu Fayzullina <alsufayzi@gmail.com>
2024-08-27 23:22:36 +0300

    add db+doker

--- app/main.py
+++ app/main.py
@@ -1,4 +1,7 @@
+from contextlib import asynccontextmanager
+import logging
 import dotenv
+
 dotenv.load_dotenv()
 
 import uvicorn
@@ -8,15 +11,22 @@ from fastapi_pagination import add_pagination
 from app.routes import status, user, users
 from app.database.engine import create_db_and_tables
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logging.warning('On startup')
+    create_db_and_tables()
+    yield
+
+    logging.warning('On shutdown')
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 app.include_router(user.router)
 
-
 add_pagination(app)
 
-
 if __name__ == "__main__":
-    create_db_and_tables()
     uvicorn.run(app, host="localhost", port=8002)
